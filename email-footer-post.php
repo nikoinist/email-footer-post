0cf2fe5bcfd85d473b7aa4359331be09dc9cd8b0
nikoinist <zoran.mihelcic@gmail.com>
2016-01-28 18:52:18 +0000

    Initial commit

---
+++ email-footer-post.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Email footer
+ * Description: Add an email and a custom message at the end of the post
+ * Version: 1.0
+ * Author: Zoran Mihelcic
+ * 
+ **/
+ //Exit if Accessed Directly
+ if(!defined('ABSPATH')){
+     exit;
+ }
+ 
